78a5778ef9a3728ba5753fb1f2559958bcc4efb5
Bhimsen Padalkar <bhim.padalkar@gmail.com>
2014-10-09 22:15:11 +0530

    Populating UI to enter list of participants

--- app/src/main/java/com/rowing/coepbc/repechage/ParticipantEntryActivity.java
+++ app/src/main/java/com/rowing/coepbc/repechage/ParticipantEntryActivity.java
@@ -2,6 +2,8 @@ package com.rowing.coepbc.repechage;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class ParticipantEntryActivity extends Activity{
 
@@ -9,5 +11,17 @@ public class ParticipantEntryActivity extends Activity{
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.participant_entry);
+    int numberOfEntries = getIntent().getIntExtra("number_of_entries", 1);
+    populateInputForEntries(numberOfEntries);
+  }
+
+  private void populateInputForEntries(int numberOfEntries) {
+    LinearLayout participantsEntriesLayout = (LinearLayout) findViewById(R.id.participant_entries);
+    for(int i = 0; i < numberOfEntries; i++) {
+      LinearLayout singleParticipantLayout = (LinearLayout) getLayoutInflater().inflate(R.layout.single_participant_entry, null);
+      TextView indexView = (TextView) singleParticipantLayout.findViewById(R.id.participant_entry_index);
+      indexView.setText(String.format("%d. ",i+1));
+      participantsEntriesLayout.addView(singleParticipantLayout);
+    }
   }
 }
