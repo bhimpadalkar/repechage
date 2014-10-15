5d5d6877cd2e46eb270ef26e93b1739fc14b1c08
Bhimsen Padalkar <bhim.padalkar@gmail.com>
2014-10-15 22:29:56 +0530

    modified the logic to return number of entries

--- app/src/main/java/com/rowing/coepbc/repechage/Repechage.java
+++ app/src/main/java/com/rowing/coepbc/repechage/Repechage.java
@@ -1,8 +1,5 @@
 package com.rowing.coepbc.repechage;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,16 +7,35 @@ import java.util.List;
 public class Repechage implements Serializable {
   private List<Participant> participants;
   private int numberOfLanes;
-  private int numberOfEntries;
+  private int numberOfParticipants;
   private List<Race> racesPlayedInLastRound;
 
-  public Repechage(int numberOfLanes, int numberOfEntries) {
+  public Repechage(int numberOfLanes, int numberOfParticipants) {
     this.numberOfLanes = numberOfLanes;
-    this.numberOfEntries = numberOfEntries;
+    this.numberOfParticipants = numberOfParticipants;
     this.participants = new ArrayList<Participant>();
   }
 
   public int getNumberOfEntries() {
+    int numberOfEntries = 0;
+    switch (numberOfLanes){
+      case 2:
+        if(numberOfParticipants <= 4)
+          numberOfEntries = 4;
+        else if(numberOfParticipants <= 8)
+          numberOfEntries = 8;
+        else numberOfEntries = 16;
+        break;
+      case 3:
+        numberOfEntries = ((numberOfParticipants + 2) / 3) * 3;
+        break;
+      case 4:
+        if(numberOfParticipants <= 12)
+          numberOfEntries = ((numberOfParticipants + 1 ) / 2) * 2;
+        else
+          numberOfEntries = ((numberOfParticipants + 3) / 4) * 4;
+        break;
+    }
     return numberOfEntries;
   }
 
